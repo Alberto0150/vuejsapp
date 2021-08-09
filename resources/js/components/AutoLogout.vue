b70c6b2e693c078a3b950e7eed5481fc41d67f45
Alberto0150 <albertosanjaya2@gmail.com>
2021-08-09 15:26:55 +0700

    Commit

---
+++ resources/js/components/AutoLogout.vue
@@ -0,0 +1,36 @@
+<template>
+    
+</template>
+<script>
+    export default{
+        data: function () {
+            return{
+                events: ['click', 'mousemove', 'scroll', 'keypress', 'load'],
+
+                theTimer:null,
+            }
+        },
+
+        mounted() {
+            this.events.forEach(function (event_name){
+                window.addEventListener(event_name, this.reset);
+            }, this
+            )},
+
+        methods:{
+            setTimer: function(){
+                this.theTimer = setTimeout(this.LogoutUser, 20 * 1000);
+            },
+
+            LogoutUser:function(){
+                $("#logout-form").submit();
+                alert('You Have been Logout');
+
+            },
+            reset: function(){
+                clearTimeout(this.theTimer);
+                this.setTimer();
+            }
+        }
+    }
+</script>
---
+++ resources/views/tablecustom.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+{{-- to be set in content, we need to wrap with @section('<name>') --}}
+@section('content')
+<div class="app">
+    <custom-table></custom-table>
+</div>
+    
+@endsection
